33426d5a36cfa17b53fddc7d3d385ceec1972e11
Viachaslau Kulbitski <fixrapdok@gmail.com>
2022-07-07 18:39:14 +0300

    Update index.ts

--- webpack-typescript/src/index.ts
+++ webpack-typescript/src/index.ts
@@ -1,13 +1,15 @@
 import func from './module/index';
 import './assets/img/rs_school.svg';
-import image1 from './assets/img/image.png';
+import image from './assets/img/image.png';
 import './global.scss';
 
-let img: HTMLImageElement = document.createElement('img');
-img.src = image1;
-img.height = 400;
 let body: HTMLElement = document.querySelector('body')!;
+let img: HTMLImageElement = document.createElement('img');
 let info: HTMLElement = document.createElement('div');
+
+img.src = image;
+img.height = 400;
 info.innerHTML = `<div>${func()}</div><div>SCSS compiler works!</div><div>Webpack version 8.4.0</div>`;
+
 body.append(img);
 body.append(info);
